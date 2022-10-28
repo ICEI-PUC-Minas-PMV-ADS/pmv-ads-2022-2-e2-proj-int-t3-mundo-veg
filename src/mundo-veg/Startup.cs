0536475b36653ab2a484459da6c207b4ddc3a794
Daiane Procópio <100447878+procopiodaiane@users.noreply.github.com>
2022-10-27 21:32:06 -0300

    Add files via upload

---
+++ src/mundo-veg/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using mundo_veg.Models;
+
+namespace mundo_veg
+{
+    public class Startup
+    {
+       public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options => {
+                    options.AccessDeniedPath = "/UsuarioPfs/AccessDenied/";
+                    options.LoginPath = "/UsuarioPfs/Login/";
+                    options.AccessDeniedPath = "/UsuarioPjs/AccessDenied/";
+                    options.LoginPath = "/UsuarioPjs/Login/";
+                });
+
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+               
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
