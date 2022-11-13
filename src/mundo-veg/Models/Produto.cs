5d9500c87ab0495796b97fa1db0810914b190999
Daiane Procópio <1244910@sga.pucminas.br>
2022-11-13 14:00:41 -0300

    feat: validação de campos Receita e Produto

--- src/mundo-veg/Models/Produto.cs
+++ src/mundo-veg/Models/Produto.cs
@@ -9,18 +9,21 @@ namespace mundo_veg.Models
         [Key]
         public int Id { get; set; }
 
+        [StringLength(40, MinimumLength = 2, ErrorMessage = "Este campo precisa ter entre 2 e 40 caracteres!")]
         [Required(ErrorMessage = "É necessário informar o nome!")]
         public string? Nome { get; set; }
-    
+
         [Display(Name = "Quantidade (unidades)")]
         [Required(ErrorMessage = "É necessário informar a quantidade!")]
-        public int Quantidade { get; set; }
+        public int? Quantidade { get; set; }
 
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "Este campo precisa ter entre 10 e 500 caracteres!")]
         [Required(ErrorMessage = "É necessário informar os ingredientes!")]
         public string? Ingredientes { get; set; }
 
         [Required(ErrorMessage = "É necessário informar o preço!")]
         [Display(Name = "Preço (R$)")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Este campo precisa ter entre 3 e 10 caracteres!")]
         public string? Preco { get; set; }
 
         [Required(ErrorMessage = "É necessário informar a categoria!")]
--- src/mundo-veg/Models/Receita.cs
+++ src/mundo-veg/Models/Receita.cs
@@ -10,13 +10,16 @@ namespace mundo_veg.Models
         [Key]
         public int Id { get; set; }
 
+        [StringLength(40, MinimumLength = 2, ErrorMessage = "Este campo precisa ter entre 2 e 40 caracteres!")]
         [Required(ErrorMessage = "É necessário informar o nome!")]
         public string? Nome { get; set; }
 
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "Este campo precisa ter entre 10 e 500 caracteres!")]
         [Required(ErrorMessage = "É necessário informar os ingredientes!")]
         public string? Ingredientes { get; set; }
 
         [Display(Name = "Modo de preparo")]
+        [StringLength(1000, MinimumLength = 10, ErrorMessage = "Este campo precisa ter entre 10 e 1000 caracteres!")]
         [Required(ErrorMessage = "É necessário informar o modo de preparo!")]
         public string? Modo_Preparo { get; set; }
 
